d5a407ca901003c445395d0641d2d3a4eeee45c2
Eric Ding <ericdingg16@outlook.com>
2022-04-23 22:12:47 +0800

    update

--- Models-codes/1运筹学/1规划论/线性规划/linear.m
+++ Models-codes/1运筹学/1规划论/线性规划/linear.m
@@ -6,5 +6,29 @@ beq = 7;
 [x, y] = linprog(f, a, b, aeq, beq, zeros(3,1));
 x,y = -y;
 
+%% 投资的收益和风险
+
+r = [5, 28, 21, 23, 25] * 0.01;
+q = [0, 2.5, 1.5, 5.5, 2.6] * 0.01;
+p = [0, 1, 2, 4.5, 6.5] * 0.01;
+M = 1;
+for a = 0:0.001:0.05
+    f = -(r - p)';
+    A = diag(q/M);
+    b = a * ones(5, 1);
+    Aeq = 1 + p;
+    beq = M;
+    lb = zeros(5,1);
+    [x, Q] = linprog(f, A, b, Aeq, beq, lb);
+    Q = -Q;
+    plot(a, Q, '*k');
+    hold on;
+end 
+xlabel('a');
+ylabel('Q');
+
+
+
+
 
 
